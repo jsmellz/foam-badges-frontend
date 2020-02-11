e4eb12d135845eb3b0a431f426e597254f88007e
jacob small <jcbssmall@gmail.com>
2020-02-11 12:58:48 -0800

    build

---
+++ build/precache-manifest.ab258c4dde67264e9a90113f766c7e1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9207e52d0c118598dab4389f11a9f471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de0a54b68da016586646",
+    "url": "/static/css/main.c54fddee.chunk.css"
+  },
+  {
+    "revision": "f83a905df7f5dc7f8435",
+    "url": "/static/js/2.ee080d49.chunk.js"
+  },
+  {
+    "revision": "3485ab9d7e60eaa2526183cd66975968",
+    "url": "/static/js/2.ee080d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0a54b68da016586646",
+    "url": "/static/js/main.39fb785f.chunk.js"
+  },
+  {
+    "revision": "a322b50437d30bfa6685",
+    "url": "/static/js/runtime-main.69fdf654.js"
+  },
+  {
+    "revision": "9e53188eb662dc1a041112caaf34aec4",
+    "url": "/static/media/bronze.9e53188e.png"
+  },
+  {
+    "revision": "197ef64432de7a7d11471bff354d35e4",
+    "url": "/static/media/pilgrim.197ef644.png"
+  },
+  {
+    "revision": "6f2aca9c2d7794e388dd17efcb13cc8b",
+    "url": "/static/media/shopaholic.6f2aca9c.png"
+  },
+  {
+    "revision": "9e53188eb662dc1a041112caaf34aec4",
+    "url": "/static/media/silver.9e53188e.png"
+  },
+  {
+    "revision": "9c132de415f6fac01c8e12c278ddf270",
+    "url": "/static/media/tastemaker.9c132de4.png"
+  }
+]);
