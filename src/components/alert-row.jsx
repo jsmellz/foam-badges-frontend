ea5a7feb40d06a5f74064b18ed64b026867ee2b0
jacob small <jcbssmall@gmail.com>
2020-02-21 19:40:35 -0800

    to aws

--- src/components/alert-row.jsx
+++ src/components/alert-row.jsx
@@ -12,7 +12,7 @@ const AlertRow = props => {
         break;
         case "wrongNetwork":
               
-            return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
+        return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
             <div className="Alert-Row" style={{backgroundColor:"#2f80ed"}}> 
                           <button style={{color:"white", backgroundColor:"2f80ed"}} className="Alert-Button">Please change your network the main Ethereum network.</button>
                       </div>
@@ -20,7 +20,7 @@ const AlertRow = props => {
         break;
         case "unlockAccount":
             
-            return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
+        return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
             <div className="Alert-Row" style={{backgroundColor:"#2f80ed"}}> 
                           <button style={{color:"white", backgroundColor:"2f80ed"}} className="Alert-Button">Please unlock your account.</button>
                       </div>
@@ -28,15 +28,14 @@ const AlertRow = props => {
         break;
         case "installMetaMask":
           
-            return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
+        return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
             <div className="Alert-Row" style={{backgroundColor:"#2f80ed"}}> 
-                         {/* <a href="https://metamask.io/" target="_blank"></a> */}
                             <a href="https://metamask.io/" style={{color:"white", backgroundColor:"2f80ed"}} className="Alert-Button">Install Metamask to see your Foam badges.</a>
                         </div>
                     </div>
           break;
         case "no-discourse":
-            return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
+        return  <div className="Row-Container" style={{backgroundColor:"transparent", borderColor:"transparent"}}>
             <div className="Alert-Row" style={{backgroundColor:"#27AF60"}}> 
                             <button style={{color:"#27AF60", backgroundColor:"white"}} className="Alert-Button">Add Discourse Username</button>
                         </div>
@@ -46,6 +45,5 @@ const AlertRow = props => {
             return <div/>;
       }
 }
-  
 
 export default AlertRow
